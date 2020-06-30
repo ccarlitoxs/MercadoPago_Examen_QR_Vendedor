6ce50098bf457ff32fc110a21b79cadb716363b0
ccarlitoxs <ccarlos_ivan@hotmail.com>
2020-06-29 22:07:39 -0300

    Mirando si escribe en el txt

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -10,7 +10,7 @@ global $access_token,$collector_id,$notificationJSON;
 // Para ver el estado del pago.
 
 $lastResource = file_get_contents('../notifications.txt');
-
+return $lastResource;
 $url= "https://api.mercadopago.com/merchant_orders/[ID]?access_token=$access_token";
 
 // REVISAR AQUÍ:
