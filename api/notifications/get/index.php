29c7bba53d6afdad871555856203c869d711cf60
ccarlitoxs <ccarlos_ivan@hotmail.com>
2020-06-30 00:23:00 -0300

    una mas daleee

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -11,14 +11,14 @@ global $access_token,$collector_id,$notificationJSON;
 
 $lastResource = file_get_contents('../notifications.txt');
 $lastResource1 = file_get_contents('../notif.txt');
-return var_dump($lastResource.$lastResource1);
-// $url= "https://api.mercadopago.com/merchant_orders/$lastResource?access_token=$access_token";
+// return var_dump($lastResource.$lastResource1);
+$url= "https://api.mercadopago.com/merchant_orders/$lastResource?access_token=$access_token";
 
 // REVISAR AQUÍ:
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
 
 // Sustituye el método por su correspondiente: get, put, post, delete
 
-// curl_call("get","$url","");
+curl_call("get","$url","");
 
  ?>
